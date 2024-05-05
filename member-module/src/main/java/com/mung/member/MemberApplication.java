ff85c766a685183a25d24738284971105b3a0b5c
Julia Song <julja.song@gmail.com>
2024-05-05 23:45:08 +0900

    [ADD] Redis setting

--- member-module/src/main/java/com/mung/member/MemberApplication.java
+++ member-module/src/main/java/com/mung/member/MemberApplication.java
@@ -2,8 +2,10 @@ package com.mung.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class MemberApplication {
 
     public static void main(String[] args) {
