67ca9101fced9aecd5f3edb52df72fd6a3b748c9
Julia Song <julja.song@gmail.com>
2024-05-08 14:23:30 +0900

    [ADD] 공통 Exception 처리 추가
    add
    - common
        - CommonException.java
        - ErrorResponse.java
        - ExceptionController.java

---
+++ common/src/main/java/com/mung/common/exception/CommonException.java
@@ -0,0 +1,27 @@
+package com.mung.common.exception;
+
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public abstract class CommonException extends RuntimeException {
+
+    public final Map<String, String> validation = new HashMap<>();
+
+    public CommonException(String message) {
+        super(message);
+    }
+
+    public CommonException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public abstract int getStatusCode();
+
+    public void addValidation(String fieldName, String message) {
+        validation.put(fieldName, message);
+    }
+
+}
